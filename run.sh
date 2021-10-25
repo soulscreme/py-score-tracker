263eb7eb5f8c3fb944158a1a08b24f8149374f9e
LJ Katz <soulscreme@gmail.com>
2021-10-25 18:19:43 +0100

    Latest updates

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export NODE_ENV=production
+export FLASK_APP=scoreApi.py
+export HOST=0.0.0.0
+
 (cd py-score-tracker-backend && nohup flask run > backend.log.txt 2>&1 &)
 (cd score-tracker-react-ui && nohup yarn start > frontend.log.txt 2>&1 &)
